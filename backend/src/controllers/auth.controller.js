40abfb763d7847f52f9e715b42d7bfc1554bad28
RobertOLtean314 <olteanrobertadrian314@gmail.com>
2025-02-22 13:56:59 +0200

    working on protectiong Routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -46,10 +46,44 @@ export const signup = async (req, res) => {
   } catch (error) {}
 };
 
-export const login = (req, res) => {
-  res.send("Signup route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePicture: user.profilePicture,
+    });
+  } catch (error) {
+    console.log("Error in login controller" + error.message);
+    res.status(500).json({ message: "Internal server error}" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("Signup route");
+  try {
+    res.cookie("jwt_token", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller" + error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  console.log("Update profile controller");
 };
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.protectRoute.jwt_token;
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {}
+};
